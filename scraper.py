bf1539c50e974ecbbfc71df4496ead77d2961b9a
Richard Hill <richardqhill@gmail.com>
2019-04-13 08:56:06 -0400

    style updates

--- scraper.py
+++ scraper.py
@@ -7,33 +7,33 @@ from selenium.webdriver.support import expected_conditions as EC # available sin
 class WhatsAppScraper:
     def __init__(self):
         self.driver = None
-        self.startDriver()
-        self.groupChats = self.grabGroupChatNames()
+        self.start_driver()
+        self.groupChats = self.grab_group_chat_names()
 
-    def startDriver(self):
+    def start_driver(self):
         self.driver = webdriver.Chrome('./chromedriver_mac')
         self.driver.get('https://web.whatsapp.com/')
 
         try:
-            element = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.ID, "app")))
+            WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.ID, "app")))
         except:
-            print("Yo, you didn't scan it fast enough....")
+            print("Yo, you didn't scan it fast enough (10 seconds)....")
 
     def quit(self):
         self.driver.quit()
 
-    def grabGroupChatNames(self):
+    def grab_group_chat_names(self):
         elems = self.driver.find_elements(By.XPATH,'//*[contains(concat( " ", @class, " " ), concat( " ", "_25Ooe", " " ))]//*[contains(concat( " ", @class, " " ), concat( " ", "_1wjpf", " " ))]')
         groupChatNames = []
-        # TODO: This doesn't actually work yet
+
         for elem in elems:
             groupChatNames.append(elem.text)
-            
+
         return groupChatNames
 
 
 if __name__ == "__main__":
     scraper = WhatsAppScraper()
-    print(scraper.grabGroupChatNames())
+    print(scraper.grab_group_chat_names())
 
 
