90aa1790c198f99b33130a0ae0ef6f3428dbd1d4
Richard Hill <richardqhill@gmail.com>
2019-04-13 09:38:08 -0400

    Add skeleton

--- scraper.py
+++ scraper.py
@@ -8,7 +8,7 @@ class WhatsAppScraper:
     def __init__(self):
         self.driver = None
         self.start_driver()
-        self.groupChats = self.grab_group_chat_names()
+        self.group_chat_names = self.grab_group_chat_names()
 
     def start_driver(self):
         self.driver = webdriver.Chrome('./chromedriver_mac')
@@ -29,9 +29,31 @@ class WhatsAppScraper:
             group_chat_names.append(elem.text)
         return group_chat_names
 
+    def select_thread(self, thread_name):
+
+        elem = self.driver.find_element_by_link_text(thread_name)
+
+        print("debug")
+
+        pass
+
+
+
+    def grab_message_bodies_from_thread(self, thread_name):
+
+        self.select_thread(thread_name)
+
+        elems = self.driver.find_elements(By.XPATH,'//*[contains(concat( " ", @class, " " ), concat( " ", "invisible-space", " " ))]')
+
+        print("debug")
+
 
 if __name__ == "__main__":
     scraper = WhatsAppScraper()
-    print(scraper.grab_group_chat_names())
+
+    temp = scraper.group_chat_names
+    print(temp)
+
+    #scraper.grab_message_bodies_from_thread(temp[0])
 
 
