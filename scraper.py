dc7ddefb3f8128b066b566baa70855f2da253d6c
Richard Hill <richardqhill@gmail.com>
2019-04-13 11:14:06 -0400

    Hard code click working

--- scraper.py
+++ scraper.py
@@ -8,40 +8,39 @@ class WhatsAppScraper:
     def __init__(self):
         self.driver = None
         self.start_driver()
-        self.group_chat_names = self.grab_group_chat_names()
+        self.group_chat_elems, self.group_chat_names = self.grab_group_chats()
 
     def start_driver(self):
         self.driver = webdriver.Chrome('./chromedriver_mac')
         self.driver.get('https://web.whatsapp.com/')
 
-        try:
-            WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.ID, "app")))
-        except:
-            print("Yo, you didn't scan it fast enough (10 seconds)....")
+        WebDriverWait(self.driver, 20).until(EC.presence_of_element_located((By.ID, "app")))
 
     def quit(self):
         self.driver.quit()
 
-    def grab_group_chat_names(self):
-        elems = self.driver.find_elements(By.XPATH,'//*[contains(concat( " ", @class, " " ), concat( " ", "_25Ooe", " " ))]//*[contains(concat( " ", @class, " " ), concat( " ", "_1wjpf", " " ))]')
+    def grab_group_chats(self):
+
+
+        element = self.driver.find_elements(By.CSS_SELECTOR, '#pane-side > div > div > div > div:nth-child(1) > div > div > div._3j7s9 > div._2FBdJ > div._25Ooe > span')
+        element[0].click()
+
+        return
+
+        self.group_chat_elems = self.driver.find_elements(By.XPATH,'//*[contains(concat( " ", @class, " " ), concat( " ", "_3j7s9", " " ))]')
+
+        text_elems = self.driver.find_elements(By.XPATH,'//*[contains(concat( " ", @class, " " ), concat( " ", "_25Ooe", " " ))]//*[contains(concat( " ", @class, " " ), concat( " ", "_1wjpf", " " ))]')
         group_chat_names = []
-        for elem in elems:
+        for elem in text_elems:
             group_chat_names.append(elem.text)
         return group_chat_names
 
-    def select_thread(self, thread_name):
-
-        elem = self.driver.find_element_by_link_text(thread_name)
-
-        print("debug")
-
-        pass
 
 
+    def grab_message_bodies_from_thread(self):
 
-    def grab_message_bodies_from_thread(self, thread_name):
-
-        self.select_thread(thread_name)
+        return
+        self.group_chat_elems[0].click()
 
         elems = self.driver.find_elements(By.XPATH,'//*[contains(concat( " ", @class, " " ), concat( " ", "invisible-space", " " ))]')
 
@@ -50,10 +49,12 @@ class WhatsAppScraper:
 
 if __name__ == "__main__":
     scraper = WhatsAppScraper()
+    scraper.grab_group_chats()
 
-    temp = scraper.group_chat_names
-    print(temp)
+    # scraper.grab_message_bodies_from_thread()
 
-    #scraper.grab_message_bodies_from_thread(temp[0])
+    scraper.quit()
+
+
 
 
